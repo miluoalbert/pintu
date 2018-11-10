ed029342fac281b07c16933c2a353fae37516f18
tingcheng <tingcheng@tingchengs-MacBook-Pro.local>
2018-11-10 17:10:42 +0800

    [fix]: set a lager template upload limit

--- application/controllers/admin/Upload.php
+++ application/controllers/admin/Upload.php
@@ -52,7 +52,7 @@ class Upload extends Controller
     {
         $config['upload_path'] = UPLOAD_PATH . 'templates' . DIRECTORY_SEPARATOR;
         $config['allowed_types'] = 'gif|jpg|png|jpeg';
-        $config['max_size'] = 100;
+        $config['max_size'] = 5120;
         $config['max_width'] = 1024;
         $config['max_height'] = 768;
         $config['encrypt_name'] = true;
